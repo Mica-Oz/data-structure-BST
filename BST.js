67691a3ae889427ad2f8cd6863a4d0ecbe05f368
Mica Oz <kahulaoz@gmail.coml>
2022-06-20 23:25:31 -0400

    insert method and classes

--- BST.js
+++ BST.js
@@ -2,6 +2,35 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(val) {
+    let newNode = new Node(val);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (val === current.val) return undefined;
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+
+    return this;
+  }
 }
 
 class Node {
@@ -11,3 +40,9 @@ class Node {
     this.right = null;
   }
 }
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
