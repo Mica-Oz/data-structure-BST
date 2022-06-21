8586202a475732e4796b14a0a0a45c978fb51889
Mica Oz <kahulaoz@gmail.coml>
2022-06-20 22:16:04 -0400

    ok

---
+++ BST.js
@@ -0,0 +1,13 @@
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+//tree terminology
+// root - top node of a tree
+//child - a node directly connected to another node when moving away from the root
+//parent - the converse notion of a child
+//siblings - a group of nodes with the same parent
+//leaf - a node with no children
+// edge - the connection between one node and another
+
+//use cases
+//-HTML DOM
+//-network routing
+//-abstract syntax tree
+//-artificial intelligence (decision tree)
+//-folders in operating systems
+
+//BINARY TREES
+//each node only has 2 children -- left and right
+
+//BINARY SEARCH TREES
+//each node only has 2 children -- left and right
+//ordered
+//numbers less than are to the left of parent node
+//numbers greater than are to the right of parent node
+
+//the way that we order them makes it very easy and fast to look things up and insert
+//searching is quicker, < or > logic at the nodes
